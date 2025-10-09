180a88ca7afe0f8b378bac1dd64021af187fc4cd
Jay Kim <csjay@snu.ac.kr>
2025-10-09 15:59:25 +0900

    [remove venv/]

--- jay-test/hf.py
+++ jay-test/hf.py
@@ -1,1 +1,6 @@
 from sentence_transformers import SentenceTransformer
+
+model_name = "sentence-transformers/bge-m3"
+cache_dir = "./models/bge-m3"
+
+model = SentenceTransformer("upskyy/bge-m3-korean", cache_folder=cache_dir)
