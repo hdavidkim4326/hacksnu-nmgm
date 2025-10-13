0755d0d7d79d916b5eeb3b4d9cc9aa9dc0aaad06
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-10-13 17:52:27 +0900

    wsgi 수정

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# Vercel이 실행할 'app' 변수를 명시적으로 추가합니다.
+app = application
