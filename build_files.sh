800259d6271f36817feeb7236b2daeb6bd532a0c
hdavidkim4326 <hdavidkim4326@gmail.com>
2025-10-13 16:04:09 +0900

    Update22 build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# Vercel 환경에 맞게 python3.9 -m pip로 수정
-python3.9 -m pip install -r requirements.txt
-
-python3.9 manage.py collectstatic --noinput
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
