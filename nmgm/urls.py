e7821a2609045adae713d3b64c2e88cff3671619
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-10-13 17:46:47 +0900

    view 수정

--- nmgm/urls.py
+++ nmgm/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('user_detail',views.user_detail, name='user_detail'),
-    path('',views.landing, name='landing'),
+
+    path('',views.landing_view, name='landing'),
 ]
+
