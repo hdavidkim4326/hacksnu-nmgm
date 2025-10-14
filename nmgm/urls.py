cab2e8ea3150f8ae1605abf84f8b75ba974ba106
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-10-14 20:51:39 +0900

    feat prototype 개발

--- nmgm/urls.py
+++ nmgm/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     path('',views.landing_view, name='landing'),
+    path('prototype/',views.prototype_view, name='prototype'),
+    path('report/',views.report_view, name='report'),
 ]
 
--- nmgm/views.py
+++ nmgm/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # 랜딩 페이지만을 보여주는 뷰 함수입니다.
 def landing_view(request):
     return render(request, 'nmgm/landing.html')
+
+def prototype_view(request):
+    return render(request, 'nmgm/prototype.html')
+def report_view(request):
+    return render(request, 'nmgm/report.html')
